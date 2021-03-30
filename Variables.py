54c92a397842bb0546802cc30e7bfed244c917d9
Adam_Codes <76964302+Adam0290@users.noreply.github.com>
2021-03-30 01:57:07 +0100

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,16 @@
+fav_drink ="Coffee"
+print(fav_drink)
+
+today
+
+breakfast = "english tea"
+lunch = "toast"
+dinner = "corn flakes"
+
+print(f"this morning for breakfast I drank {breakfast}, after that I ate {lunch}, then for tea I had {dinner}.")
+
+
+
+
+
+
